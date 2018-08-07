6e3cce715451e4a914721ff303d3bd30de71b094
knmcguire <k.n.mcguire@tudelft.nl>
2018-08-07 11:30:50 +0200

    [set up] Set up files for onboard wall following

---
+++ src/lib/wallfollowing_multiranger_onboard/wallfollowing_multiranger_onboard.c
@@ -0,0 +1,13 @@
+/*
+ * wall_follower_multi_ranger_onboard.c
+ *
+ *  Created on: Aug 7, 2018
+ *      Author: knmcguire
+ */
+
+#include "wallfollowing_multiranger_onboard.h"
+
+void testRange(float front_range, float right_range, float left_range)
+{
+	printf("range %f %f %f\n",front_range, right_range, left_range);
+}
---
+++ src/lib/wallfollowing_multiranger_onboard/wallfollowing_multiranger_onboard.h
@@ -0,0 +1,15 @@
+/*
+ * wallfollowing_multirange_onboard.h
+ *
+ *  Created on: Aug 7, 2018
+ *      Author: knmcguire
+ */
+
+#ifndef SRC_WALLFOLLOWING_MULTIRANGER_ONBOARD_H_
+#define SRC_WALLFOLLOWING_MULTIRANGER_ONBOARD_H_
+#include <stdint.h>
+#include <stdbool.h>
+
+void testRange(float front_range, float right_range, float left_range);
+
+#endif /* SRC_WALLFOLLOWING_MULTIRANGER_ONBOARD_H_ */
