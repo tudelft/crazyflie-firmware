752ef268051b42345c53c7937f847722b6cad189
knmcguire <k.n.mcguire@tudelft.nl>
2019-11-09 12:29:40 +0100

    changes to fix submodule problem

---
+++ tools/make/config.mk
@@ -0,0 +1,10 @@
+#CFLAGS += -DDECK_FORCE=bcStereoboard:bcUpDownRange
+#CFLAGS += -DDECK_FORCE=bcStereoboard
+
+#CLOAD_CMDS = -w radio://0/40/250K/E7E7E7E702
+
+#CFLAGS += -DDECK_FORCE=bcFlow:bcMultiranger
+
+#IGNORE_OW_DECKS = 1
+#ESTIMATOR=kalman
+#DEBUG=1
