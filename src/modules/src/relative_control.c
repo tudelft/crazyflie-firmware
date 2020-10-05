2122837bf24993b6be4321526451dec05dd76fbd
Bart Duisterhof <b.p.duisterhof@student.tudelft.nl>
2020-10-05 12:19:19 +0200

    working wall follower

--- src/modules/src/relative_control.c
+++ src/modules/src/relative_control.c
@@ -19,25 +19,30 @@
 
 // static float RAD2DEG = 57.29578049;
 // static float critical_laser = 0.5; // no laser ranger should ever see lower than this
-static float undesired_laser = 1.0; // start correcting if a laser ranger sees smaller than this
+static float desired_laser = 1.5; // start correcting if a laser ranger sees smaller than this
 static float desired_velocity = 0.5; // speed in m/s that we aim for
 
 static bool isInit;
 static bool onGround = true;
 static bool keepFlying = false;
+// static bool wall_following = false;
 static setpoint_t setpoint;
 static float_t relaVarInCtrl[NumUWB][STATE_DIM_rl];
 static float_t inputVarInCtrl[NumUWB][STATE_DIM_rl];
 static uint8_t selfID;
 static float_t height;
 
+float lasers[4];
 static float relaCtrl_p = 2.0f;
 static float relaCtrl_i = 0.0001f;
 static float relaCtrl_d = 0.01f;
 static float wp_reached_thres = 0.2; // [m]
 // static float NDI_k = 2.0f;
 static char c = 0; // monoCam
-float search_range = 4.0; // search range in meters
+float search_range = 10.0; // search range in meters
+
+float min_laser = 10.0f;
+int laser_decision;
 
 struct Point
 {
@@ -92,7 +97,7 @@ int decide_direction(float* d, float yaw )
     left_laser_id = 0;
   }
   // there's no danger in the moving direction
-  if (d[left_laser_id] > undesired_laser && d[right_laser_id] > undesired_laser )
+  if (d[left_laser_id] > desired_laser && d[right_laser_id] > desired_laser )
   {
     return 2;
   }
@@ -109,8 +114,6 @@ int decide_direction(float* d, float yaw )
 
 }
 
-
-
 struct Point agent_pos,goal, random_point;
 
 static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, float yawrate)
@@ -126,7 +129,6 @@ static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, 
   setpoint->velocity_body = true;
   commanderSetSetpoint(setpoint, 3);
 }
-
 
 static void flyRandomIn1meter(void){
   float_t rand_x = (rand()/(float)RAND_MAX)*(1.0f)-0.5f;
@@ -179,20 +181,19 @@ void relativeControlTask(void* arg)
   static uint32_t ctrlTick;
   systemWaitStart();
   // height = (float)selfID*0.1f+0.2f;
-  height = 0.5f;
-  float min_laser = 10.0f;
-  float lasers[4];
+  height = 1.5f;
+
   float heading = 0.f;
   float vx = 0.f;
   float vy = 0.f;
   float wp_dist = 0.f;
-  int laser_decision;
+
   getDistances(lasers);
   while(1) {
     vTaskDelay(10);
-
+    getDistances(lasers);
     keepFlying = command_share(selfID, keepFlying);
-    DEBUG_PRINT("%d %d \n", keepFlying,relativeInfoRead((float_t *)relaVarInCtrl, (float_t *)inputVarInCtrl) );
+    // DEBUG_PRINT("%d %d \n", keepFlying,relativeInfoRead((float_t *)relaVarInCtrl, (float_t *)inputVarInCtrl) );
     // if(relativeInfoRead((float_t *)relaVarInCtrl, (float_t *)inputVarInCtrl) && keepFlying){
     if(keepFlying){
       // take off
@@ -218,51 +219,39 @@ void relativeControlTask(void* arg)
         random_point.x = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range;
         random_point.y = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range;
         goal = random_point;
-        float accumulator_obs_avoidance = 0.0f;
+        // float accumulator_obs_avoidance = 0.0f;
 
         for (int i = 0; i< 70; i++) //time before time-out
         {
           estimatorKalmanGetEstimatedPos(&state); //read agent state from kalman filter
           relativeInfoRead((float_t *)relaVarInCtrl, (float_t *)inputVarInCtrl); //get relative state from other agents
           getDistances(lasers); // get laser ranger readings, order: front, left, back, right
-          min_laser = get_min(lasers); // get minimum value of laser rangers
+          // min_laser = get_min(lasers); // get minimum value of laser rangers
           agent_pos.x = state.x;
           agent_pos.y = state.y;
           wp_dist = get_distance_points(agent_pos,random_point);
 
           if ( wp_dist > wp_reached_thres)
           {
-          heading = atan2f((goal.y-agent_pos.y),(goal.x-agent_pos.x)); // heading in deg (ENU) to desired waypoint
+ 
+
+          float heading_accumulator = 0.0f;
+          //avoid obstacle
+          // for all lasers
+          for (int i = 0; i<4; i++ )
+          {
+            float laser_val = lasers[i];
+            if (laser_val < desired_laser)
+            {
+              heading_accumulator += 2.0f*powf((desired_laser-laser_val),2);
+            }
+          }
+          heading = atan2f((goal.y-agent_pos.y),(goal.x-agent_pos.x)) + heading_accumulator; // heading in deg (ENU) to desired waypoint
           vx = desired_velocity*cosf(heading);
           vy = desired_velocity*sinf(heading);
 
-          //avoid obstacle
-          if (min_laser < undesired_laser)
-          {
-            laser_decision = decide_direction(lasers,(heading+accumulator_obs_avoidance));
-            if (laser_decision != 3)
-            {
-              if (laser_decision ==0)
-              {
-                accumulator_obs_avoidance +=  0.15f;
-                heading += accumulator_obs_avoidance ;
-                vx = cosf(heading)*desired_velocity;
-                vy = sinf(heading)*desired_velocity;
-              }
-              else if (laser_decision == 1)
-              {
-                accumulator_obs_avoidance -=  0.15f;
-                heading += accumulator_obs_avoidance ;
-                vx = cosf(heading)*desired_velocity;
-                vy = sinf(heading)*desired_velocity;
-              }
-            }
-            
-          }
-          else
-          {
-            accumulator_obs_avoidance = 0.0f;
-          }
+
+
           
           // else
           // {
@@ -320,8 +309,16 @@ PARAM_ADD(PARAM_UINT8, keepFlying, &keepFlying)
 PARAM_ADD(PARAM_FLOAT, relaCtrl_p, &relaCtrl_p)
 PARAM_ADD(PARAM_FLOAT, relaCtrl_i, &relaCtrl_i)
 PARAM_ADD(PARAM_FLOAT, relaCtrl_d, &relaCtrl_d)
+
 PARAM_GROUP_STOP(relative_ctrl)
 
 LOG_GROUP_START(mono_cam)
 LOG_ADD(LOG_UINT8, charCam, &c)
 LOG_GROUP_STOP(mono_cam)
+
+LOG_GROUP_START(lasers)
+LOG_ADD(LOG_FLOAT,front,&lasers[0])
+LOG_ADD(LOG_FLOAT,left,&lasers[1])
+LOG_ADD(LOG_FLOAT,back,&lasers[2])
+LOG_ADD(LOG_FLOAT,right,&lasers[3])
+LOG_GROUP_STOP(lasers)
