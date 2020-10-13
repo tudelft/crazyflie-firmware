cfbefc93e741a76ca01d3d6c905a2437c04dca36
Bart Duisterhof <b.p.duisterhof@student.tudelft.nl>
2020-10-13 11:02:23 +0200

    bug fixes

--- src/modules/src/relative_control.c
+++ src/modules/src/relative_control.c
@@ -28,7 +28,7 @@ static float desired_velocity = 0.5; // speed in m/s that we aim for
 static bool isInit;
 static bool onGround = true;
 static bool keepFlying = false;
-static int heighest_reached = 0;
+
 // static bool follow_x = true;
 // static bool wall_following = false;
 static setpoint_t setpoint;
@@ -41,13 +41,15 @@ static int max_turns = 2;
 static int free_lasers[4];
 static int start_laser = 0;
 static int previous_free_laser = 0;
+static bool turn_positive = true;
+static int heighest_reached = 0;
+
 float lasers[4];
 static float relaCtrl_p = 2.0f;
 static float desired_heading = 0.0f;
 static float relaCtrl_i = 0.0001f;
 static float relaCtrl_d = 0.01f;
 static float wp_reached_thres = 0.2; // [m]
-static bool turn_positive = true;
 static float all_RS[NumUWB];
 static float voltage_bias[NumUWB] = {0.0,0.0,0.0};
 static float RS_lp[NumUWB] = {0.0,0.0,0.0};
@@ -62,7 +64,12 @@ float r_p, r_g, v_x, v_y;
 float rand_p = 0.1;
 float omega = 0.5;
 float phi_p = 0.5;
-float phi_g = 2.5;
+float phi_g = 0.5;
+float old_vx = 0.0;
+float old_vy = 0.0;
+
+bool fly_repulsion = true;
+float laser_repulsion_thresh = 2.0;
 
 struct Point
 {
@@ -77,7 +84,9 @@ struct gas_Point
     float gas_conc;
 };
 
-struct Point agent_pos,goal, random_point;
+struct Point agent_pos,random_point;
+struct Point goal = {.x = 0.0, .y= 0.0};
+
 struct gas_Point agent_best = {.x = 0.0f, .y=0.0f, .gas_conc = 0.0f};
 struct gas_Point swarm_best = {.x = 0.0f, .y=0.0f, .gas_conc = 0.0f};
 
@@ -152,8 +161,8 @@ static void setHoverSetpoint(setpoint_t *setpoint, float vx, float vy, float z, 
 {
   setpoint->mode.z = modeAbs;
   setpoint->position.z = z;
-  setpoint->mode.yaw = modeVelocity;
-  setpoint->attitudeRate.yaw = yawrate;
+  setpoint->mode.yaw = modeAbs;
+  setpoint->attitude.yaw = yawrate;
   setpoint->mode.x = modeVelocity;
   setpoint->mode.y = modeVelocity;
   setpoint->velocity.x = vx;
@@ -165,8 +174,8 @@ static void setHover_pos_Setpoint(setpoint_t *setpoint, float vx, float vy, floa
 {
   setpoint->mode.z = modeAbs;
   setpoint->position.z = z;
-  setpoint->mode.yaw = modeVelocity;
-  setpoint->attitudeRate.yaw = yawrate;
+  setpoint->mode.yaw = modeAbs;
+  setpoint->attitude.yaw = yawrate;
   setpoint->mode.x = modeAbs;
   setpoint->mode.y = modeAbs;
   setpoint->position.x = vx;
@@ -233,9 +242,19 @@ void update_wps(void)
   {
     if ( RS_lp[i] > swarm_best.gas_conc)
     {
-      swarm_best.gas_conc = RS_lp[i];
-      swarm_best.x = agent_pos.x + relaVarInCtrl[i][STATE_rlX];
-      swarm_best.y = agent_pos.y + relaVarInCtrl[i][STATE_rlY];
+      if (i != selfID)
+      {
+        swarm_best.gas_conc = RS_lp[i];
+        swarm_best.x = agent_pos.x + relaVarInCtrl[i][STATE_rlX];
+        swarm_best.y = agent_pos.y + relaVarInCtrl[i][STATE_rlY];
+      }
+      else
+      {
+        swarm_best.gas_conc = RS_lp[i];
+        swarm_best.x = agent_pos.x; 
+        swarm_best.y = agent_pos.y;
+      }
+      
     }
   }
 
@@ -297,9 +316,10 @@ void relativeControlTask(void* arg)
         agent_pos.x = state.x;
         agent_pos.y = state.y;
 
+        update_wps();
         // compute next wp
-        random_point.x = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range + agent_pos.x;
-        random_point.y = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range + agent_pos.y;
+        random_point.x = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range;
+        random_point.y = (rand()/(float)RAND_MAX)*search_range-0.5f*search_range;
 
         r_g = (rand()/(float)RAND_MAX);
         r_p = (rand()/(float)RAND_MAX);
@@ -320,11 +340,15 @@ void relativeControlTask(void* arg)
           // min_laser = get_min(lasers); // get minimum value of laser rangers
           agent_pos.x = state.x;
           agent_pos.y = state.y;
-          wp_dist = get_distance_points(agent_pos,random_point);
+          wp_dist = get_distance_points(agent_pos,goal);
           
+          if (wp_dist < wp_reached_thres )
+          {
+            break;
+          }
 
           // if some other member of the swarm is in close proximity
-          bool fly_repulsion = true;
+          
           if (fly_repulsion)
           {
             // attraction to source
@@ -333,20 +357,20 @@ void relativeControlTask(void* arg)
             vy = desired_velocity*sinf(heading);
 
             // repulsion from other agents
-              for (int i = 0; i<NumUWB; i++)
+            for (int i = 0; i<NumUWB; i++)
+            {
+              if ( i != selfID)
               {
-                if ( i != selfID)
+                float distance = sqrtf(powf(relaVarInCtrl[i][STATE_rlX],2) + powf(relaVarInCtrl[i][STATE_rlY],2));
+                if (distance < 2.0f)
                 {
-                  float distance = sqrtf(powf(relaVarInCtrl[i][STATE_rlX],2) + powf(relaVarInCtrl[i][STATE_rlY],2));
-                  if (distance < 1.5f)
-                  {
-                    float heading_to_agent = atan2f(relaVarInCtrl[i][STATE_rlY],relaVarInCtrl[i][STATE_rlX]);
-                    float repulsion_heading = heading_to_agent + (float)(M_PI);
-                    vx+= 5.0f*powf((1.5f-distance),2)*cosf(repulsion_heading);
-                    vy+= 5.0f*powf((1.5f-distance),2)*sinf(repulsion_heading);
-                  }       
-                }
+                  float heading_to_agent = atan2f(relaVarInCtrl[i][STATE_rlY],relaVarInCtrl[i][STATE_rlX]);
+                  float repulsion_heading = heading_to_agent + (float)(M_PI);
+                  vx+= 5.0f*powf((2.0f-distance),2)*cosf(repulsion_heading);
+                  vy+= 5.0f*powf((2.0f-distance),2)*sinf(repulsion_heading);
+                }       
               }
+            }
 
             // repulsion from lasers
             for (int i = 0; i < 4; i++)
@@ -363,7 +387,21 @@ void relativeControlTask(void* arg)
             float vector_size = sqrtf(powf(vx,2) + powf(vy,2));
             vx = vx/vector_size*desired_velocity;
             vy = vy/vector_size*desired_velocity;
+            
+            vx = 0.9f*old_vx + 0.1f*vx;
+            vy = 0.9f*old_vy + 0.1f*vy;
+            old_vx = vx;
+            old_vy = vy;
+
             setHoverSetpoint(&setpoint,vx,vy,height,0);
+
+            // float effective_heading = atan2f(vy,vx);
+            // if (abs(effective_heading-desired_heading)>M_PI_2)
+            // {
+            //   break;
+            // }
+
+            
           }
           else
           {
@@ -496,6 +534,15 @@ void relativeControlTask(void* arg)
 
             }
 
+            fly_repulsion = false;
+            for (int i = 0; i < 4; i++)
+            {
+              if (lasers[i] < laser_repulsion_thresh)
+              {
+                fly_repulsion = true;
+              }
+            }
+
             heading = M_PI_2*first_free_laser;
 
             // heading = atan2f((goal.y-agent_pos.y),(goal.x-agent_pos.x)); // heading in deg (ENU) to desired waypoint
@@ -507,14 +554,13 @@ void relativeControlTask(void* arg)
           else
           {
             setHoverSetpoint(&setpoint,0,0,height,0);
-          }
-          
-          vTaskDelay(M2T(100));         
+          }     
 
           }
-          
+        vTaskDelay(M2T(100));    
         }
-      
+
+        
       } 
     }
     else{
@@ -556,11 +602,22 @@ LOG_GROUP_START(lasers)
 LOG_ADD(LOG_FLOAT,gas0,&all_RS[0])
 LOG_ADD(LOG_FLOAT,gas1,&all_RS[1])
 LOG_ADD(LOG_FLOAT,gas2,&all_RS[2])
-LOG_ADD(LOG_FLOAT,gas0_lp,&RS_lp[0])
-LOG_ADD(LOG_FLOAT,gas1_lp,&RS_lp[1])
-LOG_ADD(LOG_FLOAT,gas2_lp,&RS_lp[2])
+
 LOG_ADD(LOG_FLOAT,front,&lasers[0])
 LOG_ADD(LOG_FLOAT,left,&lasers[1])
 LOG_ADD(LOG_FLOAT,back,&lasers[2])
 LOG_ADD(LOG_FLOAT,right,&lasers[3])
+
 LOG_GROUP_STOP(lasers)
+
+LOG_GROUP_START(PSO)
+LOG_ADD(LOG_FLOAT,goal_x,&goal.x)
+LOG_ADD(LOG_FLOAT,goal_y,&goal.y)
+LOG_ADD(LOG_FLOAT,agent_x,&agent_pos.x)
+LOG_ADD(LOG_FLOAT,agent_y,&agent_pos.y)
+LOG_ADD(LOG_FLOAT,swarm_best_x,&swarm_best.x)
+LOG_ADD(LOG_FLOAT,swarm_best_y,&swarm_best.y)
+LOG_ADD(LOG_FLOAT,gas0_lp,&RS_lp[0])
+LOG_ADD(LOG_FLOAT,gas1_lp,&RS_lp[1])
+LOG_ADD(LOG_FLOAT,gas2_lp,&RS_lp[2])
+LOG_GROUP_STOP(PSO)
