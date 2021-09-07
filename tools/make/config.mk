c696d88b5b31debd9d83048c7eed10c39e86c505
matejkarasek <matejkarasek@gmail.com>
2021-09-07 18:15:55 +0200

    Cleanup config, add descriptions

--- tools/make/config.mk
+++ tools/make/config.mk
@@ -1,38 +1,51 @@
-## Config for Flapper Drone Nimble+
-###################################################
+##########################################
+#### Config for Flapper Drone Nimble+ ####
+##########################################
 
 POWER_DISTRIBUTION = nimble_FD_PCB
 
-# CFLAGS += -DNIMBLE_USE_CF2 % only when using CF2 as flight controller
+## Uncomment when using CF2 as flight controller
+# CFLAGS += -DNIMBLE_USE_CF2
+
+## Do not change the settings below, unless if you know waht you are doing ##
+#############################################################################
 
 CFLAGS += -DMOTOR_SETUP_NIMBLE
 CFLAGS += -DENABLE_PWM_EXTENDED
 CFLAGS += -DPID_FILTER_ALL
-
-CFLAGS += -DIMU_PHI=0.0f
+CFLAGS += -DCPPM_USE_PA3 # CPPM pin, other alternatives:  PA7 (default), PA2(TX2), PA3(RX2), PB4(IO_3), PB5(IO_2) or PB8(IO_1)
+CFLAGS += -DIMU_PHI=0.0f # IMU orientation
 CFLAGS += -DIMU_THETA=90.0f
 CFLAGS += -DIMU_PSI=180.0f
 
+## Deck selection ##
+####################
+
+## Uncomment the in-built features ("virtual" decks) you'd like to use:
+# CFLAGS += -DDECK_FORCE=bcCPPM # force the CCPM deck
+# CFLAGS += -DDECK_FORCE=bcUSD # force the SD card deck
+# CFLAGS += -DDECK_FORCE=bcLedRing
+# CFLAGS += -DDECK_FORCE=bcCurrentDeck # analog current sensor on PA2/TX2 (not integrated)
+
+## To use multiple features at a time, separate their names with colons:
 # CFLAGS += -DDECK_FORCE=bcUSD:bcCPPM:bcLedRing:bcCurrentDeck
 
-# CFLAGS += -DDECK_FORCE=bcUSD # force the SD card deck
-# CFLAGS += -DDECK_FORCE=bcCurrentDeck
-
-# CFLAGS += -DIMPROVED_BARO_Z_HOLD
-# CFLAGS += -DNIMBLE_MAX_THRUST 53000.0f
-
-
-# CFLAGS += -DDECK_FORCE=bcCPPM # force the CCPM deck
-# CFLAGS += -DCPPM_USE_PA2 # use alternative pins for CPPM: PA2(TX2), PA3(RX2), PB4(IO_3), PB5(IO_2) or PB8(IO_1)
+## External receiver settings ##
+################################
 # CFLAGS += -DEXTRX_ALT_HOLD # enable altitude hold with external Rx
 # CFLAGS += -DEXTRX_ARMING # enable arming with external Rx (setup via "Brushless handling" compile flags)
 # CFLAGS += -DEXTRX_TAER # use TAER channel mapping instead of the default AETR - Aileron(Roll), Elevator(Pitch), Thrust, Rudder(Yaw)
 
-# CFLAGS += -DDECK_FORCE=bcLedRing
+## RGB LED settings ##
+######################
 # CFLAGS += -DTURN_OFF_LED
-# CFLAGS += -DLEDRING_DEFAULT_EFFECT=19
-# CFLAGS += -DLED_RING_NBR_LEDS=20
+# CFLAGS += -DLEDRING_DEFAULT_EFFECT=7
+# CFLAGS += -DLED_RING_NBR_LEDS=12
 
-# CFLAGS += -DDECK_FORCE=bcUSD:bcLedRing # force the SD card deck and the LED Ring deck
+## Better baro hold with no additional sensors ##
+#################################################
+# CFLAGS += -DIMPROVED_BARO_Z_HOLD
 
-###############################################
+## Limit maximal thrust (100% = 65535) ##
+#########################################
+# CFLAGS += -DNIMBLE_MAX_THRUST 53000.0f
